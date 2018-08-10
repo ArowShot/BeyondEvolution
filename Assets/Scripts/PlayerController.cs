3c7a243b16778d05838beb974e46de57575e0066
Conway <mjconway1@labs.neit.edu>
2018-08-10 18:23:16 -0400

    Added fluid movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+    public float xAxis;
+    public float yAxis;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called independent of framerate - physics code goes here
+    void FixedUpdate()
+    {
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal") * speed;
+        yAxis = Input.GetAxisRaw("Vertical") * speed;
+
+        Vector2 velocityX = rb.velocity;
+        velocityX.x = xAxis;
+        rb.velocity = velocityX;
+
+        Vector2 velocityY = rb.velocity;
+        velocityY.y = yAxis;
+        rb.velocity = velocityY;
+
+        if (rb.velocity.magnitude > speed)
+        {
+            rb.velocity *= speed / rb.velocity.magnitude;
+        }
+    }
 }

acf0c55e88fb38e579a0d89b3cba55edf3b027f1
Conway <mjconway1@labs.neit.edu>
2018-08-11 01:21:32 -0400

    Added LaserAttack

---
+++ Assets/Scripts/LaserAttackTrait.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserAttackTrait : Trait, IActiveTrait {
+    public int StaminaRequired
+    {
+        get { return 1; }
+    }
+    public override string Name
+    {
+        get { return "Lasers Trait"; }
+    }
+    public override int Cost
+    {
+        get { return 3; }
+    }
+    public override Environment Environment
+    {
+        get { return Environment.Forest; }
+    }
+
+    public void DoActive(PlayerController player)
+    {
+        ShootLasers();
+    }
+
+    private void ShootLasers()
+    {
+
+    }
+}

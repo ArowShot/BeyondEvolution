22541c84ff08afde4806b771266c3e73243823cf
Kyle <arow411@gmail.com>
2018-08-11 14:48:36 -0400

    added menu

---
+++ Assets/Scripts/CLickAnywehere.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CLickAnywehere : MonoBehaviour {
+	public void doClick()
+	{
+		SceneManager.LoadScene("TraitsScene");
+	}
+}

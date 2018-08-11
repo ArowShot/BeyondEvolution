41450002c394055f2084b60f98756494a0bde284
Conway <mjconway1@labs.neit.edu>
2018-08-10 23:09:26 -0400

    Added enemy logic

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
+    private Rigidbody2D _rb;
+    public float MinDistance = 4f;
+    public float Range;
+    public Transform Target;
+    public float Speed = 1;
+    public float Health = 50;
+    public float Defense = 0;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        _rb = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// Update is called independent of framerate - puts physics logic in here
+	void FixedUpdate ()
+    {
+        EnemyPathfinding();
 	}
+
+    void EnemyPathfinding()
+    {
+        Range = Vector2.Distance(transform.position, Target.position);
+
+        if (Range < MinDistance)
+        {
+            Debug.Log(Range);
+            transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+        }
+    }
 }
